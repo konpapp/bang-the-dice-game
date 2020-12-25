16a60817cc67b2acb581a958231277a59e50ea1f
kon_pa <kwnstantinos.pz@gmail.com>
2020-12-25 18:55:24 +0100

    distribute roles

--- public/client.js
+++ public/client.js
@@ -1,3 +1,17 @@
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+let roles = ['sheriff', 'renegade', 'outlaw', 'outlaw'];
+
 $(document).ready(function () {
   /* Global io */
   let socket = io();
@@ -19,9 +33,9 @@ $(document).ready(function () {
         $(`#pos${counter}`).removeClass('pos-border').addClass('pos-border-rdy');
       }
     }
-    if (data.currentUsers === 1) {
-      $('#num-users').text(data.currentUsers + ' user online');
-    } else { $('#num-users').text(data.currentUsers + ' users online'); }
+    if (Object.keys(data.loggedUsers).length === 1) {
+      $('#num-users').text('1 user online');
+    } else { $('#num-users').text(Object.keys(data.loggedUsers).length + ' users online'); }
     let message = data.name + (data.connected ? ' has joined the chat.' : ' has left the chat.');
     $('#messages').append($('<li>').html('<b>' + message + '</b>'));
   });
@@ -35,6 +49,19 @@ $(document).ready(function () {
       $(`#pos${data.posNum}`).removeClass('pos-border').addClass('pos-border-rdy');
     } else {
       $(`#pos${data.posNum}`).removeClass('pos-border-rdy').addClass('pos-border');
+    }
+    let counter = 0;
+    let allReady = true;
+    for (let user in data.loggedUsers) {
+      counter++;
+      if (!data.loggedUsers[user]) {
+        allReady = false;
+        break;
+      }
+    }
+    if (allReady && counter >= 4) {
+      $('#num-users').text('Game is starting!');
+      socket.emit('start game', data.loggedUsers);
     }
   })
 
@@ -50,6 +77,42 @@ $(document).ready(function () {
   $('#rdy-form').submit(function () {
     socket.emit('ready button');
     return false;
+  })
+
+  // Start the game
+  socket.on('start game', (users) => {
+    switch (users.length) {
+      case 5:
+        roles.push('deputy');
+        break;
+      case 6:
+        roles.push('deputy');
+        roles.push('outlaw');
+        break;
+      case 7:
+        roles.push('deputy');
+        roles.push('outlaw');
+        roles.push('deputy');
+        break;
+      case 8:
+        roles.push('deputy');
+        roles.push('outlaw');
+        roles.push('deputy');
+        roles.push('renegade');
+        break;
+    }
+    roles = shuffle(roles);
+    let players = [];
+    let playerObj = {};
+    for (let i=0; i < users.length; i++) {
+      playerObj.name = users[i];
+      playerObj.role = roles[i];
+      playerObj.health = 5;
+      players.push(playerObj);
+      if (roles[i] == 'sheriff') {
+        $('#num-users').text(users[i] + ' is Sheriff.');
+      }
+    }
   })
 
 });
